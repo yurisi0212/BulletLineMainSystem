cd1ac39ae51828f18929981bc3c51575baaa8a97
sqmeru-meruchan.rb <64207094+sqmeru@users.noreply.github.com>
2020-07-13 23:30:38 +0900

    Add files via upload
    ・commandMapの説明欄を更新
    ・入室退出メッセージを更新

--- BulletLineMainSystem/src/sqmeru/BulletLineMainSystem/BulletLineMainSystem.php
+++ BulletLineMainSystem/src/sqmeru/BulletLineMainSystem/BulletLineMainSystem.php
@@ -4,12 +4,32 @@ namespace sqmeru\BulletLineMainSystem;
 use pocketmine\plugin\PluginBase;
 use sqmeru\BulletLineMainSystem\Command\CommandMap;
 
-class BulletLineMainSystem extends PluginBase
+use pocketmine\Player;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+
+
+class BulletLineMainSystem extends PluginBase implements Listener
 {
     public function onEnable(){
         $this->getLogger()->info("§d読み込みました。");
         $this->getLogger()->info("てきとういうな。めやそんはJSだ");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         CommandMap::registerCommands();
+    }
+
+    public function onJoin(PlayerJoinEvent $event){
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $event->setJoinmessage("§l§a【入室】§r§e" . $name . " Joined the game");
+    }
+
+    public function onQuit(PlayerQuitEvent $event){
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $event->setQuitMessage("§l§b【退出】§r§e" . $name . " Lefted the game");
     }
 
 }
