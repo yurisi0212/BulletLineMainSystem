bc0428a05651ab1bc4f1f1e477d21d935fa814b0
yurisi0212 <Dev.yurisi@gmail.com>
2020-07-14 23:43:18 +0900

    リファクタリング (#3)
    Co-authored-by: yurisi0212 <surarin0212@gmail.com>

--- BulletLineMainSystem/src/sqmeru/BulletLineMainSystem/BulletLineMainSystem.php
+++ BulletLineMainSystem/src/sqmeru/BulletLineMainSystem/BulletLineMainSystem.php
@@ -2,34 +2,19 @@
 namespace sqmeru\BulletLineMainSystem;
 
 use pocketmine\plugin\PluginBase;
+
 use sqmeru\BulletLineMainSystem\Command\CommandMap;
-
-use pocketmine\Player;
-
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
+use sqmeru\BulletLineMainSystem\Event\EventManager;
 
 
-class BulletLineMainSystem extends PluginBase implements Listener
-{
+class BulletLineMainSystem extends PluginBase{
+
     public function onEnable(){
         $this->getLogger()->info("§d読み込みました。");
         $this->getLogger()->info("てきとういうな。めやそんはJSだ");
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("ところでめやそん小学校通ってないみたいなんですけどだいじょうぶなんですかね");
+        EventManager::registerEvents($this);
         CommandMap::registerCommands();
-    }
-
-    public function onJoin(PlayerJoinEvent $event){
-        $player = $event->getPlayer();
-        $name = $player->getName();
-        $event->setJoinmessage("§l§a【入室】§r§e" . $name . " Joined the game");
-    }
-
-    public function onQuit(PlayerQuitEvent $event){
-        $player = $event->getPlayer();
-        $name = $player->getName();
-        $event->setQuitMessage("§l§b【退出】§r§e" . $name . " Lefted the game");
     }
 
 }
